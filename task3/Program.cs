0388d7955b304ee69dd50f7c532230cf45741727
Vitalie-Olexici <olexici.dev@gmail.com>
2024-01-06 16:55:42 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        ReverseArrayPrint(array, array.Length - 1);
+    }
+Main();
+
+void ReverseArrayPrint(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            ReverseArrayPrint(array, index - 1);
+        }
+    }
